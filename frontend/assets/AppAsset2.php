7a50ac3da86eb63255c7913f1bdffc74248d03f9
Sardor <S.yuldashev@ingo.uz>
2023-09-18 09:23:32 +0500

    har bir role uchun layatular ochildi

---
+++ frontend/assets/AppAsset2.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        'css/fonts/boxicons.css',
+//        'css/core.css',
+//        'css/theme-default.css',
+//        'css/demo.css',
+//        'css/libs/perfect-scrollbar/perfect-scrollbar.css',
+//        'css/libs/apex-charts/apex-charts.css',
+    ];
+    public $js = [
+//        'js/helpers.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapAsset',
+    ];
+}
